def241921237f7deba5917c999dffc9ebd434d03
allardy <yann.allard1@gmail.com>
2022-08-11 02:21:55 -0400

    tst

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import fs from 'fs'
+import mkdirp from 'mkdirp'
+
+const run = () => {
+  mkdirp.sync('bin')
+  fs.writeFileSync('bin/file.exe', 'lol')
+  fs.writeFileSync('bin/file2.exe', 'lol')
+}
+
+run()
