06bf23b858ccf3e9b4f6399206eddd7d6b70f43a
allardy <yann.allard1@gmail.com>
2022-08-11 02:23:44 -0400

    de

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const run = () => {
   mkdirp.sync('bin')
   fs.writeFileSync('bin/file.exe', 'lol')
   fs.writeFileSync('bin/file2.exe', 'lol')
+  fs.writeFileSync('bin/file3.exe', 'lol')
 }
 
 run()
